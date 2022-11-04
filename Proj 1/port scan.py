fe64028dcaf8c1a6090d3926a830a1f9257cd8c6
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-11-04 21:21:33 +0330

    files

--- Proj 1/port scan.py
+++ Proj 1/port scan.py
@@ -23,10 +23,33 @@ def scan_ports(host, start, last):
                     print(item)
 
 
-if __name__ == '__main__':
-    while True:
-        addr = input('Please Enter Your IP/Domain: ')
-        begin_port = input('Please Enter Your starting port number: ')
-        end_port = input('Please Enter Your ending port number: ')
-        scan_ports(addr, begin_port, end_port)
-        sleep(3)
+import socket
+
+
+def isOpen(hostname, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.1)
+    result = sock.connect_ex((hostname, port))
+    sock.close()
+    return result == 0
+
+
+def ipScan(host, start, end):
+    live_hosts = []
+    for i in range(start, end):
+        tmp_host = host + '.' + str(i)
+        res = isOpen(tmp_host, 23)
+        if res:
+            yield tmp_host
+
+
+for host in ipScan('89.43.3', 1, 255):
+    print(host)
+
+# if __name__ == '__main__':
+#     while True:
+#         addr = input('Please Enter Your IP/Domain: ')
+#         begin_port = input('Please Enter Your starting port number: ')
+#         end_port = input('Please Enter Your ending port number: ')
+#         scan_ports(addr, begin_port, end_port)
+#         sleep(3)
