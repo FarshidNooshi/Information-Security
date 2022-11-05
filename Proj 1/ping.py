5fc8cb162303e8ab8574d3ac629bedab5ba031d7
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-11-05 12:04:24 +0330

    done with ping.py

--- Proj 1/ping.py
+++ Proj 1/ping.py
@@ -1,10 +1,14 @@
 import os
 from time import sleep
 
+import pyfiglet
+
 SAVE_DIR = '/results/result_ping.txt'
+NUMBERS_OF_PINGS = 4
 
 if __name__ == '__main__':
+    print(pyfiglet.figlet_format('Ping checker'))
     while True:
         addr = input('IP/Domain: ')
-        out = os.system(f'ping {addr} -t 4')
+        out = os.system(f'ping {addr} -t {NUMBERS_OF_PINGS}')
         sleep(3)
