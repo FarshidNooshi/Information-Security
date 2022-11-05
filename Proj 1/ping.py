7a370af06667d403c9eaddae0cf8a3df8b763ea7
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-11-05 11:36:52 +0330

    edits

--- Proj 1/ping.py
+++ Proj 1/ping.py
@@ -1,18 +1,10 @@
 import os
 from time import sleep
 
-from ping3 import verbose_ping
-
 SAVE_DIR = '/results/result_ping.txt'
-
-
-def ping_server(server: str):
-    """ping server"""
-    return verbose_ping(server)
-
 
 if __name__ == '__main__':
     while True:
         addr = input('IP/Domain: ')
-        out = ping_server(addr)
+        out = os.system(f'ping {addr} -t 4')
         sleep(3)
--- Proj 1/port scan.py
+++ Proj 1/port scan.py
@@ -1,6 +1,7 @@
 import os
 
 import nmap
+import pyfiglet as pyfiglet
 
 SAVE_DIR = '/results/result_port scan.txt'
 
@@ -11,24 +12,29 @@ def scan_ports(host_addr, start_port_num, last_port_num):
     curr_path = os.path.dirname(os.path.abspath(__file__))
     with open(curr_path + SAVE_DIR, 'w') as f:
         for host in host_scan.all_hosts():
-            print(f'state of the host: {host_scan[host].state()}')
+            print(f"state of the host({host_scan[host]['hostnames'][0]['name']}): {host_scan[host].state()}")
             for protocol in host_scan[host].all_protocols():
                 print(f'protocol: {protocol}')
                 for port in host_scan[host][protocol].keys():
                     if host_scan[host][protocol][port]['state'] == 'open':
-                        save_open_port(port, f)
+                        save_open_port(port, f, host_scan[host][protocol][port])
+                    else:
+                        print(
+                            f"port: {port}, state: {host_scan[host][protocol][port]['state']},"
+                            f" service: {host_scan[host][protocol][port]['name']}")
 
 
 def generate_range(last_port_num, start_port_num):
     return start_port_num + '-' + last_port_num
 
 
-def save_open_port(port, f):
-    print(f'open port: {port}')
-    f.write(f'open port: {port}\n')
+def save_open_port(port, f, scan_result):
+    print(f"open port: {port}, service: {scan_result['name']}")
+    f.write(f"open port: {port}, service: {scan_result['name']}")
 
 
 if __name__ == '__main__':
+    print(pyfiglet.figlet_format('Port Scanner'))
     host = input('Host IP: ')
     start_port = input('Start port: ')
     last_port = input('last port: ')
