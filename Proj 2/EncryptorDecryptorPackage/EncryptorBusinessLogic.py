c46aaa6f74a0a2afb05e4bbdcfd2132d6195a795
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-12-09 12:31:11 +0330

    second and third parts

--- Proj 2/EncryptorDecryptorPackage/EncryptorBusinessLogic.py
+++ Proj 2/EncryptorDecryptorPackage/EncryptorBusinessLogic.py
@@ -1,5 +1,8 @@
 import base64
+import binascii
 import os
+
+import pbkdf2
 
 
 class EncryptorBusinessLogic:
@@ -8,10 +11,35 @@ class EncryptorBusinessLogic:
 
     @staticmethod
     def generate_salt():
+        """
+        Generate a salt
+        :return: the salt
+        """
         salt = os.urandom(16)
-        return str(base64.b64encode(salt))
+        return str(base64.b64encode(salt))[2:-1]
 
     def add_salt_to_key(self):
+        """
+        Add a salt to the key
+        :return: the new key
+        """
         salt = self.generate_salt()
         self.key = self.key + salt
         return self.key
+
+    def change_key_size(self, key_size):
+        """
+        Change the key size to the given key size
+
+        :param key_size: the key size to change to (in bits)
+        :return: the new key
+        """
+        self.key = pbkdf2.PBKDF2(self.key, self.key).read(key_size // 8)
+        return self.key
+
+    def show_hex_key(self):
+        """
+        Show the key in hexadecimal
+        :return: the key in hexadecimal
+        """
+        return binascii.b2a_hex(self.key)
--- Proj 2/Main.py
+++ Proj 2/Main.py
@@ -1,7 +1,14 @@
 import os
+
 from EncryptorDecryptorPackage.EncryptorBusinessLogic import EncryptorBusinessLogic
 
 KEY_FILE_PATH = os.path.join(os.path.dirname(__file__), "EncryptorDecryptorPackage/data/key.txt")
+
+
+def separate_lines_in_terminal():
+    print(
+        "------------------------------------------------------------------------------------------------------------------------")
+
 
 if __name__ == '__main__':
     encryptor = EncryptorBusinessLogic(KEY_FILE_PATH)
@@ -10,3 +17,10 @@ if __name__ == '__main__':
     print(f"Key length: {len(key)}")
     print(f'sample salt: {encryptor.generate_salt()}')
     print(f"Key with salt: {encryptor.add_salt_to_key()}")
+    separate_lines_in_terminal()
+    print(f"Key with salt length in bytes: {len(encryptor.key)}")
+    print(f"Key with salt and key size of 256 bits: {encryptor.change_key_size(256)}")
+    print(f"Key with salt and key size of 256 bits length in bytes: {len(encryptor.key)}")
+    print(f"Key with salt and key size of 256 bits in hexadecimal: {encryptor.show_hex_key()}")
+    print(f"Key with salt and key size of 256 bits in hexadecimal length in bytes: {len(encryptor.show_hex_key())}")
+    separate_lines_in_terminal()
